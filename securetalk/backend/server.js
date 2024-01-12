b39b7d7456843a2eea4ce881630dd4f94435c326
William0987 <williamwsianto@gmail.com>
2024-01-12 05:16:40 -0500

    generate frontend structure

--- securetalk/backend/server.js
+++ securetalk/backend/server.js
@@ -8,6 +8,6 @@ app.get("/", (req, res) => {
     res.send("Working...")
 });
 
-const PORT = process.env.PORT || 3000
-app.listen(3000, console.log(`Server started on port ${PORT}`))
+const PORT = process.env.PORT || 5000
+app.listen(5000, console.log(`Server started on port ${PORT}`))
 
