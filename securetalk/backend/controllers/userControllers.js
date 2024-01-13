30ccec78eb17d9cb439ae16cb5d50d8c61718778
William0987 <williamwsianto@gmail.com>
2024-01-13 18:01:18 -0500

    implemented search user functionality and chat functionalities

--- securetalk/backend/controllers/userControllers.js
+++ securetalk/backend/controllers/userControllers.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const generateToken = require("../config/generateToken");
+const asyncHandler = require("express-async-handler");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, pic } = req.body;
@@ -58,4 +59,18 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { registerUser, authUser };
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
+
+module.exports = { registerUser, authUser, allUsers };
