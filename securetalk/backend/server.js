fd953b5d44d5b023dd6a7a52eb67d91d8e717449
William0987 <williamwsianto@gmail.com>
2024-02-04 14:20:36 -0500

    updated server.js

--- securetalk/backend/server.js
+++ securetalk/backend/server.js
@@ -5,6 +5,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -64,3 +65,17 @@ io.on("connection", (socket) => {
     socket.leave(userData._id);
   });
 });
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
